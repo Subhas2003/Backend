8072505c1eb131837051779cd01fb1a6bbcf8723
Subhas2003 <subhasmunain@gmail.com>
2025-09-07 01:19:59 +0530

    Cloudinary

---
+++ src/utils/cloundinary.js
@@ -0,0 +1,43 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+const uploadOnCloudinary = async () => {
+  // Configuration
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET
+  });
+
+  // Upload an image
+  const uploadResult = await cloudinary.uploader
+    .upload(
+      'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+        public_id: 'shoes',
+      }
+    )
+    .catch((error) => {
+      console.log(error);
+    });
+
+  console.log(uploadResult);
+
+  // Optimize delivery
+  const optimizeUrl = cloudinary.url('shoes', {
+    fetch_format: 'auto',
+    quality: 'auto'
+  });
+
+  console.log(optimizeUrl);
+
+  // Transform the image
+  const autoCropUrl = cloudinary.url('shoes', {
+    crop: 'auto',
+    gravity: 'auto',
+    width: 500,
+    height: 500,
+  });
+
+  console.log(autoCropUrl);
+};
+
+export default uploadOnCloudinary;
